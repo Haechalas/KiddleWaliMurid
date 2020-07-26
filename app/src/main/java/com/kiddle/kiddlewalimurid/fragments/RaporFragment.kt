62ba5aeb7c2988dd29f7015bf8bbfccb13dca245
Haechalas <haechalas@gmail.com>
2020-07-26 16:51:02 +0700

    update final

--- app/src/main/java/com/kiddle/kiddlewalimurid/fragments/RaporFragment.kt
+++ app/src/main/java/com/kiddle/kiddlewalimurid/fragments/RaporFragment.kt
@@ -34,7 +34,7 @@ class RaporFragment : Fragment() , View.OnClickListener {
         //untuk dropdown kelas
         val kelas = listOf("Bintang Kecil", "Bintang Besar", "Bulan Kecil", "Bulan Besar")
         val adapter_kelas = ArrayAdapter<String>(requireActivity().applicationContext, R.layout.item_dropdown_text, kelas)
-        (view.dropdown_rapor_nilai.editText as? AutoCompleteTextView)?.setAdapter(adapter_kelas)
+        (view.dropdown_rapor_kelas.editText as? AutoCompleteTextView)?.setAdapter(adapter_kelas)
 
         //set on-click listener
         view.expand_button_0.setOnClickListener(this)
