8d9870f125df22b603afcfb3c0d1fb2ef126142b
Haechalas <haechalas@gmail.com>
2020-07-26 13:37:30 +0700

    update (sisa rapihin ID)

---
+++ app/src/main/java/com/kiddle/kiddlewalimurid/UI/PengaturanActivity.kt
@@ -0,0 +1,12 @@
+package com.kiddle.kiddlewalimurid.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kiddle.kiddlewalimurid.R
+
+class PengaturanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pengaturan)
+    }
+}
